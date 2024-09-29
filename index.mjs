9f471e64f4140553d1072bed749bf758f695a57e
RahulYavvari <rahulyavvari04@gmail.com>
2024-09-29 11:20:00 +0530

    Minor bugs

--- index.mjs
+++ index.mjs
@@ -2,22 +2,22 @@ import loadDatabase from "./utils/databaseController.mjs";
 
 export const handler = async (event) => {
     const records = event.Records;
-
+    const response = null;
     for(let i = 0; i < records.length; i++) {
         const message = records[i].body;
 
         try {
             const databaseResponse = await loadDatabase(message);
             console.log("[LOG] Successfully uploaded into database!");
-            const response = {
+            response = {
                 statusCode: 200,
                 body: `Successfully uploaded into database! \n [RESPONSE] \n ${databaseResponse}`
             }
         } catch(err) {
             console.error("[LOG] Failed to upload into database", err);
-            const response = {
+            response = {
                 statusCode: 500,
-                body: `Failed to upload into database! \n [RESPONSE] \n ${databaseResponse}`
+                body: `Failed to upload into database! \n [RESPONSE] \n ${err}`
             }
         }
     }
